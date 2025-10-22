ecc16910e17a23d421dc8c576fd71d3006cd09ea
Guilherme Damasceno <gui.damascenonobre52@gmail.com>
2025-10-22 15:21:09 -0300

    creating User table and seeding with admin

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,9 @@ FROM node:22-alpine
 
 WORKDIR /app
 COPY package*.json ./
-RUN npm install && npm audit fix
+RUN npm install
 COPY . .
 
 # Use env vars from docker-compose
 EXPOSE 3000
-CMD ["npm", "run", "start:prod"]
+CMD ["npm", "run", "start"]
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRootAsync({
-      useFactory: () => ({
-        type: 'postgres',
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT
-          ? parseInt(process.env.DB_PORT, 10)
-          : undefined,
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        autoLoadEntities: true,
-        synchronize: false, // using migrations now
-      }),
-    }),
-  ],
+  imports: [ConfigModule.forRoot({ isGlobal: true })],
   controllers: [],
   providers: [],
 })
-export class AppModule { }
+export class AppModule {}
