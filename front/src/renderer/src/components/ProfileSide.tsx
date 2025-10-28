c79079d256a151d5c5aae53d57e2a526092104f0
Julian Cardoso <juliancesarcardoso@gmail.com>
2025-10-28 18:16:39 -0300

    feat: componente de sidebar da tela de edição/delete usuario

--- front/src/renderer/src/components/ProfileSide.tsx
+++ front/src/renderer/src/components/ProfileSide.tsx
@@ -8,14 +8,14 @@ export default function ProfileSide(): React.JSX.Element {
   const isActive = (path: string): boolean => location.pathname === path
 
   return (
-    <div className="max-w-80 mx-auto h-full border-r border-(--light-gray) pr-8">
-      <div className="flex flex-col gap-5 pt-5">
+    <aside className="hidden md:block w-full md:w-80 lg:w-96 shrink-0 h-full border-r border-(--light-gray) pr-4 md:pr-8">
+      <nav className="flex flex-col gap-3 md:gap-5 pt-3 md:pt-5">
         <button
           onClick={() => navigate('/profile-edit')}
-          className={`text-lg font-semibold text-left p-2 rounded-md transition-colors
+          className={`text-base md:text-lg text-(--black) font-semibold text-left p-2 rounded-md transition-colors
             ${
               isActive('/profile-edit')
-                ? 'bg-(--gray-light) text-(--black)'
+                ? 'bg-(--gray-light) text-(--black) shadow-sm'
                 : 'text-(--black) hover:bg-(--gray-light)/50'
             }`}
         >
@@ -26,16 +26,16 @@ export default function ProfileSide(): React.JSX.Element {
           onClick={() => {
             navigate('/profile-delete')
           }}
-          className={`text-lg font-semibold text-left p-2 rounded-md transition-colors
+          className={`text-base md:text-lg font-semibold text-left p-2 rounded-md transition-colors
             ${
               isActive('/profile-delete')
-                ? 'bg-(--erro)/25 text-(--erro)'
-                : 'text-(--black) hover:bg-(--erro)/50'
+                ? 'bg-(--erro)/25 text-(--erro) shadow-sm'
+                : 'text-(--erro) hover:bg-(--erro)/10'
             }`}
         >
           Excluir usuário
         </button>
-      </div>
-    </div>
+      </nav>
+    </aside>
   )
 }
